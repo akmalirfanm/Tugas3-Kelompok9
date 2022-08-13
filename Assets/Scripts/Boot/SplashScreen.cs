2e6e1ff7e77bf1e31b6edc6db10e635de678d96f
Akmal Irfan Maulana <akmalirfanm@gmail.com>
2022-08-13 14:30:10 +0700

    Initial commit

---
+++ Assets/Scripts/Boot/SplashScreen.cs
@@ -0,0 +1,21 @@
+using Agate.MVC.Base;
+using Agate.MVC.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kelompok9.SpaceInvader.Boot
+{
+    public class SplashScreen : BaseSplash<SplashScreen>
+    {
+        protected override ILoad GetLoader()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IMain GetMain()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
